3640013e1f476c1523fd1379e6a0875e314b3104
Friis <nfriis24@gmail.com>
2023-04-25 18:32:47 +0200

    Init Project

---
+++ Plantt.API/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Plantt.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+            
+        }
+    }
+}
